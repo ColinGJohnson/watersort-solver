3e90e07f5ec486b78335d49626899a64f1c97b8a
Colin Johnson <colin@colinjohnson.net>
2023-01-18 22:13:39 -0800

    Add test case for is_complete

--- src/solver/test_WaterSortProblem.py
+++ src/solver/test_WaterSortProblem.py
@@ -1,16 +1,28 @@
 from unittest import TestCase
-from WaterSortProblem import WaterSortProblem
+from WaterSortProblem import WaterSortProblem, is_complete
 
 
-class WaterSortProblem(TestCase):
+class TestWaterSortProblem(TestCase):
     def test_is_complete(self):
-        self.fail()
+
+        # Complete
+        self.assertTrue(is_complete([]))
+        self.assertTrue(is_complete([[]]))
+        self.assertTrue(is_complete([[], []]))
+        self.assertTrue(is_complete([[1], [2]]))
+        self.assertTrue(is_complete([[1, 1], [2], [3, 3, 3]]))
+
+        # Incomplete
+        self.assertFalse(is_complete([[1, 2]]))
+        self.assertFalse(is_complete([[3], [1, 1, 2]]))
+        self.assertFalse(is_complete([[2, 1, 1], [3]]))
+        self.assertFalse(is_complete([[1, 2, 3], [3, 4, 5]]))
 
     def test_get_actions(self):
-        self.fail()
+        pass
 
     def test_can_pour(self):
-        self.fail()
+        pass
 
     def test_pour(self):
-        self.fail()
+        pass
