51e34c4dad196f16fdef489e1ee4d5497dc603fc
Colin Johnson <colin@colinjohnson.net>
2023-01-29 18:34:13 -0800

    Complete controller for basic puzzles

--- watersort/controller/WaterSortController.py
+++ watersort/controller/WaterSortController.py
@@ -1,5 +1,4 @@
-from datetime import time
-from pprint import pprint
+import time
 from typing import List, Tuple
 
 import colour
@@ -8,10 +7,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyautogui
 import pygetwindow as gw
-from PIL import Image
-from numpy._typing import ArrayLike
+from numpy._typing import ArrayLike, NDArray
 
-from solver.WaterSortTypes import Action
+from solver.WaterSortTypes import Action, GameState
 
 
 def show_img(img_plt):
@@ -47,11 +45,14 @@ def find_tube_locations(img, debug=False) -> List[Tuple[int, int, int, int]]:
     return locations
 
 
-def crop_img(img, left_crop=0.05, right_crop=0.05, top_crop=0.3, bottom_crop=0.2):
+def crop_img(img, left_crop=0.05, right_crop=0.05, top_crop=0.3, bottom_crop=0.2) -> Tuple[int, int, NDArray]:
+    """Trim the edges of the input image by the specified proportions. The defaults are the values that
+    happened to work well for my phone (Pixel 7 Pro)."""
+
     left = int(img.shape[1] * left_crop)
-    right = int(img.shape[1] * 1 - right_crop)
+    right = int(img.shape[1] * (1 - right_crop))
     top = int(img.shape[0] * top_crop)
-    bottom = int(img.shape[0] * 1 - bottom_crop)
+    bottom = int(img.shape[0] * (1 - bottom_crop))
     return top, left, img.copy()[top:bottom, left:right]
 
 
@@ -65,10 +66,8 @@ def add_sample_to_palette(palette: List[ArrayLike], color: ArrayLike):
     for i, palette_color in enumerate(palette):
         delta_e = colour.delta_E(palette_color, color)
         if delta_e < delta_e_threshold:
-            print("Found existing color in palette:", i, delta_e, color, palette_color)
             return i
 
-    print("adding new color to palette:", color)
     palette.append(color)
     return len(palette) - 1
 
@@ -76,10 +75,12 @@ def add_sample_to_palette(palette: List[ArrayLike], color: ArrayLike):
 class WaterSortController:
     """Abstracts actions in the 'SortPuz' app."""
 
-    num_slices = 4
     offset_proportion = 0.15
+    window_position = {'x': 40, 'y': 10}
 
-    def __init__(self, window_title: str):
+    def __init__(self, window_title: str, num_slices=4):
+        self.controller_state = None
+        self.num_slices = num_slices
         self.window = gw.getWindowsWithTitle(window_title)[0]
 
     def restore_window(self):
@@ -87,14 +88,14 @@ class WaterSortController:
         because screenshots don't work on secondary monitors."""
         self.window.restore()
         self.window.activate()
-        self.window.moveTo(10, 10)
+        self.window.moveTo(self.window_position['x'], self.window_position['y'])
 
-    def screenshot_window(self) -> Image:
+    def screenshot_window(self) -> NDArray:
         self.restore_window()
         region = (self.window.left, self.window.top, self.window.width, self.window.height)
         return cv.cvtColor(np.array(pyautogui.screenshot(region=region)), cv.COLOR_RGB2BGR)
 
-    def update_state(self, debug=True):
+    def update_state(self, debug=True) -> GameState:
         """Screenshot the window and apply image processing to determine the current game state"""
 
         img_screenshot = self.screenshot_window()
@@ -102,13 +103,14 @@ class WaterSortController:
 
         # assume the game's background color is similar to the pixel at the top right of the cropped screenshot
         bg_color = cv.cvtColor(img_cropped.astype(np.float32) / 255, cv.COLOR_BGR2Lab)[0, 0]
+
+        # color index '0' will always represent the background color, so we can filter out empty layers
         color_palette = [bg_color]
 
         tube_locations = find_tube_locations(img_cropped)
         tubes = []
 
         for tube_location in tube_locations:
-            print("Checking tube at location (offset from cropped):", tube_location)
             x, y, w, h = tube_location
             tube_crop = img_cropped[y:y + h, x:x + w]
             tubes.append({
@@ -118,19 +120,39 @@ class WaterSortController:
 
         for tube in tubes:
             x, y, w, h = tube['tube_location']
+
+            if debug:
+                cv.rectangle(img_cropped, (x, y), (x + w, y + h), (255, 0, 255), 2)
+
             for layer in tube['layers']:
                 sample_x, sample_y = layer.get('tube_position_xy')
-                layer['crop_position'] = (left + x + sample_x, top + y + sample_y)
-                if debug:
-                    color = str(layer.get('color'))
-                    cv.drawMarker(img_screenshot, layer['crop_position'], (0, 255, 0), 2)
-                    cv.putText(img_screenshot, color, layer['crop_position'], cv.QT_FONT_NORMAL, 1, (255, 255, 255), 2, cv.LINE_AA)
-        if debug:
-            pprint(tubes)
-            show_img(img_cropped)
-            show_img(img_screenshot)
 
-        return tubes
+                # position relative to cropped area
+                layer['crop_position'] = (x + sample_x, y + sample_y)
+
+                # position relative to scrcpy window
+                layer['window_position'] = (left + layer['crop_position'][0], top + layer['crop_position'][1])
+
+                # position relative to main monitor.
+                # TODO: Not currently used by click_window_location, this can be removed
+                layer['monitor_position'] = (
+                    self.window_position['x'] + layer['crop_position'][0],
+                    self.window_position['y'] + layer['crop_position'][1]
+                )
+
+                if debug:
+                    color = str(layer.get('color_index'))
+                    cv.drawMarker(img_cropped, layer['crop_position'], (0, 255, 0), 2)
+                    cv.putText(img_cropped, color, layer['crop_position'], cv.QT_FONT_NORMAL, 1, (255, 255, 255), 2, cv.LINE_AA)
+
+        if debug:
+            show_img(img_cropped)
+
+        self.controller_state = tubes
+
+        # convert the internal state used by the controller to a state usable by the solver
+        layers = [[layer.get('color_index') for layer in tube.get('layers')] for tube in tubes]
+        return [list(filter(lambda idx: idx != 0, layer))[::-1] for layer in layers]
 
     def read_tube_layer_colors(self, color_palette, tube_crop):
         h, w, _ = tube_crop.shape
@@ -153,10 +175,18 @@ class WaterSortController:
         return layers
 
     def execute_action(self, action: Action):
-        pass
+        source_index, sink_index = action
+
+        source_location = self.controller_state[source_index]['layers'][0]['window_position']
+        self.click_window_location((source_location[0], source_location[1]))
+
+        time.sleep(0.1)
+
+        sink_location = self.controller_state[sink_index]['layers'][0]['window_position']
+        self.click_window_location((sink_location[0], sink_location[1]))
 
     def click_window_location(self, target: Tuple[int, int]):
         monitor_x = self.window.left + target[0]
         monitor_y = self.window.top + target[1]
-        pyautogui.moveTo(monitor_x, monitor_y, duration=1, tween=pyautogui.easeInOutQuad)
+        pyautogui.moveTo(monitor_x, monitor_y, duration=0.2, tween=pyautogui.easeInOutQuad)
         pyautogui.click()
--- watersort/main.py
+++ watersort/main.py
@@ -1,11 +1,23 @@
+import time
 from pprint import pprint
 
 from controller.WaterSortController import WaterSortController
+from solver.WaterSortProblem import WaterSortProblem
+from solver.WaterSortSolver import weighted_astar_search, path_actions
 
 
 def main():
     controller = WaterSortController('Pixel 7 Pro')
-    pprint(controller.update_state())
+    state = controller.update_state()
+    print("Initial State:", state)
+
+    problem = WaterSortProblem(4, state)
+    solution = path_actions(weighted_astar_search(problem))
+    print("Solution:", solution)
+
+    for action in solution:
+        controller.execute_action(action)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
