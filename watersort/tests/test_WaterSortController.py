56198ce0fd10fac218adf78fbc079009381f37f1
Colin Johnson <colin@colinjohnson.net>
2023-02-02 18:29:13 -0800

    Update unit test for controller

--- watersort/tests/test_WaterSortController.py
+++ watersort/tests/test_WaterSortController.py
@@ -11,7 +11,7 @@ class TestWaterSortController(TestCase):
         controller = WaterSortController('Test')
         print(os.getcwd())
         test_image = cv.imread('resources/test_screenshot.png', cv.IMREAD_COLOR)
-        state = controller.parse_screenshot(test_image)
+        state = controller.parse_screenshot(test_image, debug=False)
         self.assertEqual(
             [[], [], [3, 1, 2, 1], [5, 2, 4, 4], [4, 5, 5, 3], [3, 3, 6, 5], [1, 7, 4, 6], [7, 6, 6, 1], [2, 2, 7, 7]],
             state
